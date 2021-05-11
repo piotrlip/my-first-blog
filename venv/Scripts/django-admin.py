dfbe11469f33bf7667eb0f507b987b24405d2854
piotrlip <piotr.l.lipinski@gmai.com>
2021-05-11 14:21:31 +0200

    Zmieniony kod HTML na stronie1.

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\piotr\desktop\moje\programowanie\python\djangoblogproject\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
